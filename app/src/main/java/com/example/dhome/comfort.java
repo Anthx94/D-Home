b67dacd70fb58d93dde8689974ca062ed1e6ac02
Anthx94 <anthonygoncalves94@outlook.fr>
2023-01-13 15:11:44 +0100

    Script GoHome

--- app/src/main/java/com/example/dhome/comfort.java
+++ app/src/main/java/com/example/dhome/comfort.java
@@ -1,18 +1,30 @@
 package com.example.dhome;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class comfort extends Activity {
+    Button bHome;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-
-
         setContentView(R.layout.comfort);
 
+        bHome = (Button) findViewById(R.id.button_Comfort_HOME_id);
+        bHome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                goHome();
+            }
+        });
+    }
 
+    public void goHome(){
+        Intent intentHome = new Intent(this, home.class);
+        startActivity(intentHome);
     }
 }
--- app/src/main/java/com/example/dhome/security.java
+++ app/src/main/java/com/example/dhome/security.java
@@ -1,11 +1,13 @@
 package com.example.dhome;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 public class security extends Activity {
+    Button bHome;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,6 +15,17 @@ public class security extends Activity {
 
         setContentView(R.layout.security);
 
+        bHome = (Button) findViewById(R.id.button_Security_HOME_id);
+        bHome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                goHome();
+            }
+        });
+    }
 
+    public void goHome(){
+        Intent intentHome = new Intent(this, home.class);
+        startActivity(intentHome);
     }
 }
