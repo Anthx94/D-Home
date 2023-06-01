9d759e31fb4d2bb6956ed5fd11c8b1041a64a680
Anthony GONCALVES <anthonygoncalves94@outlook.fr>
2023-06-01 11:38:07 +0200

    Re-Design

--- app/src/main/java/com/example/dhome/security.java
+++ app/src/main/java/com/example/dhome/security.java
@@ -4,14 +4,23 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 
-public class security extends Activity {
+import androidx.appcompat.app.AppCompatActivity;
+
+public class security extends AppCompatActivity {
     Button bHome;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //Remove title bar
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getSupportActionBar().hide();
 
         setContentView(R.layout.security);
 
--- app/src/main/java/com/example/dhome/weather.java
+++ app/src/main/java/com/example/dhome/weather.java
@@ -4,14 +4,24 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 
-public class weather extends Activity {
+import androidx.appcompat.app.AppCompatActivity;
+
+public class weather extends AppCompatActivity {
     Button bHome;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //Remove title bar
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getSupportActionBar().hide();
+
         setContentView(R.layout.weather);
 
         bHome = (Button) findViewById(R.id.button_Weather_HOME_id);
